994e4dc27ef4b559981f122a0175ffbed7182c53
bleonardb3 <bleonardb3@gmail.com>
2017-10-25 17:06:47 -0400

    Add files via upload
    Revised to work with Titanic data.

--- DSX/Lab-3/connection.R
+++ DSX/Lab-3/connection.R
@@ -1,12 +1,12 @@
 # connection.R
 
-# Fill in connection details about your dashDB instance below.
+# Fill in connection details about your DB2 Warehouse instance below.
 
-dsn.hostname <- 'HOSTNAME HERE'
-dsn.uid <- 'USER ID HERE'
-dsn.pwd <- 'PASSWORD HERE'
+dsn.hostname <- 'fill in hostname'
+dsn.uid <- 'fill in username'
+dsn.pwd <- 'fill in password'
 dsn.database <- 'BLUDB'
 dsn.port <- '50000'
 dsn.protocol <- 'TCPIP'
 
-vetting.table = 'VETTING TABLE NAME HERE'
+titanic.table = 'fill in table with predictions'
